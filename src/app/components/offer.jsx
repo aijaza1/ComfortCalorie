e787b0750eaa36c2a1315791795926c14b999117
Azeem <aijaza@vcu.edu>
2023-07-13 17:07:13 -0400

    icons

--- src/app/components/offer.jsx
+++ src/app/components/offer.jsx
@@ -1,68 +1,56 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-const offer = () => {
+const Offer = () => {
   return (
-    <div id= 'offer' className='w-full h-screen bg-[#FFBF69]'>
-        <div className='max-w-[1240px] m-auto px-2 py-10 w-full'>
+    <div id='offer' className='w-full h-screen bg-slate-100'>
+      <div className='max-w-[1240px] m-auto px-2 py-10 w-full'>
 
-<p className='text-center text-4xl lg:text-5xl tracking-wide uppercase mt-6 mb-6 text-black'>
-     What We Offer
-</p>
+        <p className='text-center text-4xl lg:text-5xl tracking-wide uppercase mt-6 mb-6 text-[#FB8500]'>
+          What We Offer
+        </p>
 
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-16 p-10'>
 
+          {/* left */}
+          <div className='w-full shadow-xl bg-white shadow-gray-400 rounded-xl px-2 py-8 lg:px-10 lg:py-24'>
+            <div className='flex flex-col items-center justify-center text-center'>
+              <div className='text-2xl lg:text-3xl lg:leading-relaxed'>
+                <p>Determine how many calories you should be eating</p>
+              </div>
 
-<div className='grid lg:grid-cols-2 gap-10 lg:gap-16 p-10'>
+              <div className='pt-5 lg:pt-12'>
+                <Link href='/#calculator'>
+                  <button className='bg-[#FB8500] hover:bg-[#E07701] text-white font-extrabold text-xl lg:text-2xl py-3 px-4 border-2 border-black rounded-md hover:scale-105 ease-in duration-300'>
+                    Calorie Calculator
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
 
+          {/* right */}
+          <div className='w-full shadow-xl bg-white shadow-gray-400 rounded-xl px-2 py-8 lg:px-10 lg:py-24'>
+            <div className='flex flex-col items-center justify-center text-center'>
+              <div className='text-2xl lg:text-3xl lg:leading-relaxed'>
+                <p>Find nutritional facts for the foods you eat</p>
+              </div>
 
-{/* left */}
-<div className='col-span-3 lg:col-span-1 w-full shadow-xl bg-white shadow-gray-400 rounded-xl px-2 py-8 lg:px-10 lg:py-24'>
-<div className='items-center justify-center text-center'>
-    <div className='text-2xl lg:text-3xl lg:leading-relaxed'>
-        <p>Determine how many calories you should be eating</p>
+              <div className='pt-5 lg:pt-12'>
+                <Link href='/#finder'>
+                  <button className='bg-[#FB8500] hover:bg-[#E07701] text-white font-extrabold text-xl lg:text-2xl py-3 px-8 border-2 border-black rounded-md hover:scale-105 ease-in duration-300'>
+                    Calorie Finder
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
+  );
+};
 
-    <div className='pt-5 lg:pt-12'>
-        <Link href='/#calculator'>
-            <button className="bg-[#FB8500] hover:bg-[#E07701] text-white font-extrabold text-xl lg:text-2xl py-3 px-4 border-2 border-black rounded-md hover:scale-105 ease-in duration-300">
-                Calorie Calculator
-            </button>
-        </Link>
-    </div>
-</div>
-</div>
-
-
-
-{/* right */}
-<div className='col-span-3 lg:col-span-1 w-full shadow-xl bg-white shadow-gray-400 rounded-xl px-2 py-8 lg:px-10 lg:py-24'>
-<div className='items-center justify-center text-center'>
-    <div className='text-2xl lg:text-3xl lg:leading-relaxed'>
-        <p>Find nutritional facts for the foods you eat</p>
-    </div>
-
-    <div className='pt-5 lg:pt-12'>
-        <Link href='/#finder'>
-            <button className="bg-[#FB8500] hover:bg-[#E07701] text-white font-extrabold text-xl lg:text-2xl py-3 px-8 border-2 border-black rounded-md hover:scale-105 ease-in duration-300">
-                Calorie Finder
-            </button>
-        </Link>
-    </div>
-
-</div>
-</div>
-
-
-</div>
-
-
-
-</div>
-
-</div>
-
-    
-  )
-}
-
-export default offer
+export default Offer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
@@ -16,11 +15,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className="scroll-smooth"
-      style={{ scrollBehavior: "smooth" }}
-    >
+    <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
